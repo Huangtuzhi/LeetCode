28f7a067c3270fed1648bc14655cac532048ad73
unknown <titushuang@titushuang-PC0.tencent.com>
2015-06-22 20:04:45 +0800

    invert binary tree

---
+++ invertBinaryTree/invertBinaryTree.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+class Solution {
+public:
+    TreeNode* invertTree(TreeNode* root)
+    {
+    	if (root->left != NULL || root->right != NULL)
+    	{
+    		TreeNode *temp = root->left;
+    		root->left = root->right;
+    		root->right = temp;
+    		invertTree(root->left);
+    		invertTree(root->right);
+    	}
+    }
+};
+
+int main()
+{
+	TreeNode a(4);
+	TreeNode b(2);
+	TreeNode c(7);
+	TreeNode d(1);
+	TreeNode e(3);
+	TreeNode f(6);
+	TreeNode g(9);
+
+	Solution sol;
+	return 0;
+}

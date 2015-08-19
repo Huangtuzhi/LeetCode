8135de318242651a338c9d0eb01f465ba1be13a2
titus <ituzhi@163.com>
2015-08-19 21:13:05 +0800

    Update longestCommonPrefix.cpp

--- longestCommonPrefix/longestCommonPrefix.cpp
+++ longestCommonPrefix/longestCommonPrefix.cpp
@@ -15,14 +15,14 @@ public:
     	for(int j=0; j<strs[0].size(); j++)
     	{
     		for(int i=1; i<strs.size(); i++)
-   			{
-   				if (strs[0][j] != strs[i][j])
-   					return strs[0].substr(0, length);
-   			}
-   			length++;
+   		{
+   		    if (strs[0][j] != strs[i][j])
+   			return strs[0].substr(0, length);
    		}
-
-   		return strs[0].substr(0, length);
+   		length++;
+   	}
+   	
+   	return strs[0].substr(0, length);
     }
 };
 
